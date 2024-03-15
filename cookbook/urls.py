67861f0a53af66253e9f843c78be562acd42911c
BogdanMaria <maria_costinel@yahoo.com>
2024-03-15 12:54:49 +0000

    Create draft for recipe details and wire up urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='browse-recipes'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_list')
 ]
