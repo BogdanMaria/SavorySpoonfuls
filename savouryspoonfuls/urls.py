f51dcbef413ede35a0ec429ea801eb1f2ec9ddb7
BogdanMaria <maria_costinel@yahoo.com>
2024-03-15 17:18:49 +0000

    Install allauth and wire up the links

--- savouryspoonfuls/urls.py
+++ savouryspoonfuls/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("cookbook.urls"), name="cookbook-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
