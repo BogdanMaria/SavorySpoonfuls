bfd5511520545554d1cde61328c69c8680b165f9
BogdanMaria <maria_costinel@yahoo.com>
2024-03-16 13:15:02 +0000

    Create template for drafts age and urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('browserecipes/', views.RecipeList.as_view(), name='browse-recipes'),
+    path('drafts/', views.UserDrafts.as_view(), name='drafts'),
     path('mycookbook/', views.UserRecipes.as_view(), name='mycookbook'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_list'),
 ]
