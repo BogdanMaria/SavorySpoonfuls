99fe5fe8afcfc7b1f26ac2e66faaaf60262a206d
BogdanMaria <maria_costinel@yahoo.com>
2024-03-16 10:14:36 +0000

    Add views for filtering the logged in user recipes and create 'mycookbook' template

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('browserecipes/', views.RecipeList.as_view(), name='browse-recipes'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_list')
+    path('mycookbook/', views.UserRecipes.as_view(), name='mycookbook'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_list'),
 ]
